dbd16c36589ce0d4deb39dbe8de8bf9ab2c59c81
felipe7920 <157854658+felipe7920@users.noreply.github.com>
2026-05-04 22:37:38 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/REFOOD/', // <--- AGREGA ESTA LÍNEA AQUÍ (Usa el nombre exacto de tu repo)
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
